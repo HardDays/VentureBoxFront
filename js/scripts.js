af95a2d13a58a122f497f275de72f8d276718c76
snolly <snolly.gv@gmail.com>
2019-05-21 12:44:41 +0300

    -feat: add login

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
 // Empty JS for your own code to be here
+var TOKEN = '';
+if (localStorage.getItem('token')) {
+    TOKEN = localStorage.getItem('token');
+}
+
+setUserOnHeader();
+
+function setUserOnHeader() {
+    if (TOKEN) {
+        $('#login-block').css({'display':'none'});
+        $('#username-block').css({'display':'block'});
+        var user = JSON.parse(localStorage.getItem('user'));
+        $('#username-text').text(user.name + " " + user.surname);
+    }
+}
